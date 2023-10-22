a737ae887a06fbef912b9e4db3cab2a37165db2a
nadeto26 <nadezhdakarapetrova@gmail.com>
2023-10-22 17:25:57 +0300

    Test

---
+++ 02.VillainNames/01.ADO.NET/Names/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.ADO.NET.Names
+{
+    internal static class Config
+    {
+        public const string ConnectionString =
+            @"Server=.\SQLEXPRESS;Database=MinionsDB;Integrated Security=True;TrustServerCertificate=True";
+    }
+}
