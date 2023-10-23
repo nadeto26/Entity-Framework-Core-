bca7a21a36a957e6ad87c0c3cdbe62dcbf810184
nadeto26 <nadezhdakarapetrova@gmail.com>
2023-10-23 15:45:43 +0300

    Test

---
+++ 02.Exercise ORM Fundamentals/MINIORM/MiniORM/ExceptionMessages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MINIORM.MiniORM
+{
+    internal static class ExceptionMessages
+    {
+        public const string EntityNullException = "Entity cannot be null!";
+
+        public const string InvalidEntitiesException = "{0} Invalid Entities found in {1}!";
+    }
+}
---
+++ 02.Exercise ORM Fundamentals/MINIORM/MiniORMApp/Data/Entities/EmployeeProject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MINIORM.MiniORM.Data.Entities
+{
+    public class EmployeeProject
+    {
+        [Key]
+        [ForeignKey(nameof(Employee))]
+        public int EmployeeId { get; set; }
+
+        public Employee Employee { get; set; }
+
+        [Key]
+        [ForeignKey(nameof(Project))]
+        public int ProjectId { get; set; }
+
+        public Project Project { get; set; }
+    }
+}
