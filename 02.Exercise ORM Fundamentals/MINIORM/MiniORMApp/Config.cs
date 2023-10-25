30c3e3ec8c248bd7c9b0fded458a761c4f936703
nadeto26 <nadezhdakarapetrova@gmail.com>
2023-10-25 21:21:02 +0300

    Test

---
+++ 02.Exercise ORM Fundamentals/MINIORM/MiniORMApp/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MINIORM.MiniORM
+{
+    public static class Config
+    {
+        public const string ConnectionString =
+            @"Server=DESKTOP-EFFK5Q4\SQLEXPRESS;Database=MiniORM;Integrated Security=True;Encrypt=False;";
+    }
+}
---
+++ 02.Exercise ORM Fundamentals/MINIORM/MiniORMApp/Data/Entities/Department.cs
@@ -0,0 +1,21 @@
+﻿using MINIORM.MiniORM.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MINIORM.MiniORM.Data.Entityes
+{
+    public class Department
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public ICollection<Employee> Employees { get; set; }
+    }
+}
