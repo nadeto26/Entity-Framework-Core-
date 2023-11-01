07675a80993a0a0b7d915c8ee22b86da69eb2fdb
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-01 15:09:15 +0200

    Test

---
+++ 05.LINQ/03. Songs Above Duration/Data/MusicHubDbContext.cs
@@ -0,0 +1,64 @@
+﻿namespace MusicHub.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using MusicHub.Data.Models;
+
+    public class MusicHubDbContext : DbContext
+    {
+        public MusicHubDbContext()
+        {
+        }
+
+        public MusicHubDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+        public DbSet<Producer> Producers { get; set; }
+
+        public DbSet<Album> Albums { get; set; }
+
+        public DbSet<Song> Songs { get; set; }
+
+        public DbSet<Performer> Performers { get; set; }
+
+        public DbSet<Writer> Writers { get; set; }
+
+        public DbSet<SongPerformer> SongsPerformers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //за да спестим място и да не използваме пълния datetime
+            builder.Entity<Song>(entity =>
+            {
+                entity
+                    .Property(s => s.CreatedOn)
+                    .HasColumnType("date");
+            });
+
+            builder.Entity<Album>(entity =>
+            {
+                entity
+                    .Property(a => a.ReleaseDate)
+                    .HasColumnType("date");
+            });
+
+            // за да направим PerformerId and SongId pk
+            //така правим composit key
+            builder.Entity<SongPerformer>(entity =>
+            {
+                entity
+                    .HasKey(sp => new { sp.PerformerId, sp.SongId });
+            });
+
+        }
+    }
+}
