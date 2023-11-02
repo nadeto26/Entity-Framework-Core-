23db3364f1f1598a2d96f921e92d9cf37489fddc
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-02 18:00:25 +0200

    Test

--- 04.Entity Relations/01. Student System/P01_StudentSystem.Data/DbConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace P01_StudentSystem.Data.Common
-{
-    public static class DbConfig
-    {
-        public const string ConnectionString =
-          @"Server=DESKTOP-EFFK5Q4;Database=School;Integrated Security=True;Encrypt=False;";
-    }
-}
