a318cf7287f5ff424ae8403b56350a0acde5f104
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-03 17:52:35 +0200

    Test

---
+++ 06.Advanced Querying/08. Author Search/BookShop/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+            
+            string input = Console.ReadLine();
+            string resuler = GetAuthorNamesEndingIn(db, input);
+        }
+
+        public static string GetAuthorNamesEndingIn(BookShopContext dbContext, string input)
+        {
+            string[] authorNames = dbContext.Authors
+                .Where(a => a.FirstName.EndsWith(input))
+                .OrderBy(a => a.FirstName)
+                .ThenBy(a => a.LastName)
+                .Select(a => $"{a.FirstName} {a.LastName}")
+                .ToArray();
+
+            return string.Join(Environment.NewLine, authorNames);
+        }
+
+    }
+}
+
+
