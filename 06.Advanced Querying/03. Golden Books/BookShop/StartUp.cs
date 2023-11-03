cfa6e3c05cd435322c2a021e08b1ff432be860b3
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-03 17:51:48 +0200

    Test

---
+++ 06.Advanced Querying/03. Golden Books/BookShop/StartUp.cs
@@ -0,0 +1,53 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            string input = Console.ReadLine();
+            string result = GetBooksByCategory(db, input);
+        }
+
+        // Problem 03
+        public static string GetGoldenBooks(BookShopContext dbContext)
+        {
+            string[] bookTitles = dbContext.Books
+                .Where(b => b.EditionType == EditionType.Gold &&
+                            b.Copies < 5000)
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, bookTitles);
+        }
+
+        //Problem6
+        public static string GetBooksByCategory(BookShopContext context, string input)
+        {
+            // In-memory, we are still not approaching the DB
+            string[] categories = input
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(c => c.ToLower())
+            .ToArray();
+
+            string[] bookTitles = context.Books
+                .Where(b => b.BookCategories
+                    .Any(bc => categories.Contains(bc.Category.Name.ToLower())))
+                .OrderBy(b => b.Title)
+                .Select(b => b.Title)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, bookTitles);
+        }
+    }
+}
+
+
