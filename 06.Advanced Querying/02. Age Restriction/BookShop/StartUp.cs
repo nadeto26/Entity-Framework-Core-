6a37f37c2228dbe478bb2952c2025a2dd7b835b0
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-03 17:47:44 +0200

    Test

---
+++ 06.Advanced Querying/02. Age Restriction/BookShop/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            string ageRestrictionInput = Console.ReadLine();
+            string result = GetBooksByAgeRestriction(db, ageRestrictionInput);
+
+            Console.WriteLine(result);
+        }
+        // Problem 02
+        public static string GetBooksByAgeRestriction(BookShopContext dbContext, string command)
+        {
+            try
+            {
+                AgeRestriction ageRestriction = Enum.Parse<AgeRestriction>(command, true);
+
+                string[] bookTitles = dbContext.Books
+                    .Where(b => b.AgeRestriction == ageRestriction)
+                    .OrderBy(b => b.Title)
+                    .Select(b => b.Title)
+                    .ToArray();
+                return string.Join(Environment.NewLine, bookTitles);
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+    }
+}
+
+
