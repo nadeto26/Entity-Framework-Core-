7fab8aaf711e5fcaadcfc4750cd4df2ed4d11034
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-04 17:23:03 +0200

    Test

---
+++ 06.Advanced Querying/07. Released Before Date/BookShop/StartUp.cs
@@ -0,0 +1,37 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+    using System.Globalization;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            string date = Console.ReadLine();
+            string result = GetBooksReleasedBefore(db,date);
+        }
+
+        
+
+        //Problem7
+
+        public static string GetBooksReleasedBefore(BookShopContext context, string date)
+        {
+            DateTime convertedDate = DateTime.ParseExact(date, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+            var books = context.Books
+                .Where(b => b.ReleaseDate < convertedDate)
+                .OrderByDescending(b => b.ReleaseDate)
+                .Select(b => $"{b.Title} - {b.EditionType} - ${b.Price:F2}")
+                .ToList();
+
+            var result = String.Join(Environment.NewLine, books);
+            return result;
+        }
+    }
+}
+
+
