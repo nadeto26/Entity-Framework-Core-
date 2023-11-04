4df94181196129928ca8b4a7c03e66d10379860c
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-04 17:18:35 +0200

    Test

---
+++ 06.Advanced Querying/04. Books by Price/BookShop/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            string result = GetBooksByPrice(db);
+        }
+
+        public static string GetBooksByPrice(BookShopContext context)
+        {
+            string[] books = context.Books
+                .Where(x => x.Price > 40)
+                .OrderByDescending(x => x.Price)
+                .Select(a=> $"{a.Title} - ${a.Price:f2}")
+                .ToArray();
+
+            return string.Join(Environment.NewLine, books);
+        }
+    }
+}
+
+
