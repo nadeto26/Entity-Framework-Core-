a50f6618fd13577e94fb9dd7dfa22196dd71862f
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-04 17:21:04 +0200

    Test

---
+++ 06.Advanced Querying/05. Not Released In/BookShop/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            int year = int.Parse(Console.ReadLine());
+            string result = GetBooksNotReleasedIn(db,year);
+        }
+
+       
+
+        //Problem5
+
+        public static string GetBooksNotReleasedIn(BookShopContext context, int year)
+        {
+            string[] books = context.Books
+                .Where(a => a.ReleaseDate.Value.Year != year)
+                .OrderBy(a => a.BookId)
+                .Select(a=>a.Title) .ToArray();
+
+            return string.Join(Environment.NewLine, books);
+        }
+
+         
+    }
+}
+
+
