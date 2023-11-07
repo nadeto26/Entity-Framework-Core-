c5e3b81b6ccce82812d19207fe7d0637265eab82
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-07 19:30:07 +0200

    Test

---
+++ 07.Auto-Mapping objects/Auto-Mapping-Objects-Exercises-FastFood-6.0/FastFood.Models/Employee.cs
@@ -0,0 +1,34 @@
+﻿namespace FastFood.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Employee
+    {
+        public Employee()
+        {
+            
+            this.Orders = new HashSet<Order>();
+        }
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [Range(15, 80)]
+        public int Age { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public string Address { get; set; } = null!;
+
+        public int PositionId { get; set; }
+
+        public virtual Position Position { get; set; } = null!;
+
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
---
+++ 07.Auto-Mapping objects/Auto-Mapping-Objects-Exercises-FastFood-6.0/FastFoodCommon/ConnectionConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace FastFoodCommon
+{
+    public class ConnectionConfig
+    {
+        public const string ConnectionString =
+            @"Server=DESKTOP-EFFK5Q4;Database=FastFood2023;Integrated Security=True;Encrypt=False;";
+    }
+}
