4f7f69b14b779cc942851666e743a9c087195251
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-07 21:30:04 +0200

    Test

--- 07.Auto-Mapping objects/Auto-Mapping-Objects-Exercises-FastFood-6.0/FastFoodCommon/ConnectionConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FastFoodCommon
-{
-    public class ConnectionConfig
-    {
-        public const string ConnectionString =
-            @"Server=DESKTOP-EFFK5Q4;Database=FastFood2023;Integrated Security=True;Encrypt=False;";
-    }
-}
