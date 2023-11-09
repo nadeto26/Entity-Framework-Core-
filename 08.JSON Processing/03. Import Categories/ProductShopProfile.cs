eb0e634cd5c37f099eca815ae658d94bf8ab5999
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-09 17:13:20 +0200

    Test

---
+++ 08.JSON Processing/03. Import Categories/ProductShopProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using ProductShop.DTOs;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile() 
+        {
+            // Za da se sluchi mappinga 
+           
+            // User
+
+            this.CreateMap<ImportUserDTO, User>();
+
+            //Product 
+            this.CreateMap<ImportantProductDto, Product>();
+
+            //Category
+
+            this.CreateMap<ImportCategotyDto, Category>();
+        }
+    }
+}
