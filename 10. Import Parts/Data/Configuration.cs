f1c412c651a199241215fc4bb4698aece9273ec6
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-10 18:25:40 +0200

    Test

--- 10. Import Parts/Data/Configuration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CarDealer.Data
-{
-    public static class Configuration
-    {
-        public const string ConnectionString = 
-            @"Server=DESKTOP-EFFK5Q4;Database=CarDealer;Integrated Security=True;Encrypt=False";
-    }
-}
