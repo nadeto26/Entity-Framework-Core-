313e7e64a49b034f643c249a33e63afd97b54038
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-10 18:25:13 +0200

    Test

--- 08.JSON Processing/01. Import Users/ProductShop/Data/Configuration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ProductShop.Data
-{
-    public static class Configuration
-    {
-        public const string ConnectionString =
-            @"Server=DESKTOP-EFFK5Q4;Database=ProductShop;Integrated Security=True";
-    }
-}
