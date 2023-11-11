674a92f861419c4de94cdbc3f895608d9b158306
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-11 11:55:41 +0200

    Test

---
+++ 08.JSON Processing/10. Import Parts/CarDealerProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            //Supplier 
+
+            this.CreateMap<ImportSupplieDto, Supplier>();
+
+            //Part
+
+            this.CreateMap<ImportPartDto, Part>();
+        }
+    }
+}
---
+++ 08.JSON Processing/10. Import Parts/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace CarDealer.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString = 
+            @"Server=DESKTOP-EFFK5Q4;Database=CarDealer;Integrated Security=True;Encrypt=False";
+    }
+}
