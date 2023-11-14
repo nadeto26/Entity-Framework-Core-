15b1ce23050c5a2ceae048b8eed1c08925646a95
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-14 21:07:55 +0200

    Test

---
+++ 09.Extensible Markup Language - XML/13. Import Sales/DTOs/Import/ImportCarPartDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CarDealer.DTOs.Import
+{
+    [XmlType("PartId")]
+    public class ImportCarPartDto
+    {
+        //вече е атрибут , а не елемент 
+        [XmlAttribute("id")]
+        public int PartId { get; set; }
+    }
+}
