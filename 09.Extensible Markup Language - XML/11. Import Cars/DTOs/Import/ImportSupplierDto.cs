ec6185f581ed5122ff12a2d7f4b60f77d38ae555
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-14 21:07:33 +0200

    Test

---
+++ 09.Extensible Markup Language - XML/11. Import Cars/DTOs/Import/ImportSupplierDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CarDealer.DTOs.Import
+{
+    [XmlType("Supplier")]
+    public class ImportSupplierDto
+    {
+         
+         
+            [XmlElement("name")] //името на елемента , който държи стойността 
+            public string Name { get; set; } = null!;
+
+            [XmlElement("isImporter")]
+            public bool IsImporter { get; set; }
+        }
+}
---
+++ 09.Extensible Markup Language - XML/11. Import Cars/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace CarDealer.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString = @"Server=DESKTOP-EFFK5Q4;Database=CarDealer;Integrated Security=True;Encrypt=False";
+    }
+}
