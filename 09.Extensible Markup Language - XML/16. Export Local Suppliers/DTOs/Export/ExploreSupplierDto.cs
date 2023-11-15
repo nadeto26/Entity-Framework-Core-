2092e9e112c1178fb1a0530f0007429a09dc34d1
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-15 19:34:40 +0200

    Test

---
+++ 09.Extensible Markup Language - XML/16. Export Local Suppliers/DTOs/Export/ExploreSupplierDto.cs
@@ -0,0 +1,22 @@
+﻿using CarDealer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CarDealer.DTOs.Export
+{
+    [XmlType("supplier")]
+    public class ExploreSupplierDto
+    {
+        [XmlAttribute("id")]
+        public int Id { get; set; }
+
+        [XmlAttribute("name")]
+        public string Name { get; set; } = null!;
+
+        public virtual ICollection<Part> Parts { get; set; }
+    }
+}
