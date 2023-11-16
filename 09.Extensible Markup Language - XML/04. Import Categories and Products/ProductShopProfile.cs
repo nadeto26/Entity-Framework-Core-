759830b7280fc89397174c77d9f5cd821c8fab40
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-16 17:42:21 +0200

    Test

---
+++ 09.Extensible Markup Language - XML/04. Import Categories and Products/ProductShopProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+using System.Runtime.InteropServices;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            //User
+            this.CreateMap<ImportUserDto, User>();
+
+            //Product
+
+            this.CreateMap<ImportProductDto, Product>();
+
+            //Categort
+
+            this.CreateMap<ImportCategory,Category>();
+
+            //CategoriesAndProduct
+
+            this.CreateMap<ImportCategoriesAndProductsDto,CategoryProduct>();
+        }
+    }
+}
