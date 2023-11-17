fa575c939214f380f9b113cb08aa71f192a41394
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-17 18:36:06 +0200

    Test

---
+++ 09.Extensible Markup Language - XML/01. Import Users/StartUp.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using ProductShop.Data;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+using ProductShop.Utilities;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using ProductShopContext context = new ProductShopContext();
+
+            string inputXml = 
+            File.ReadAllText("../../../Datasets/users.xml");
+
+            string result = ImportUsers(context,inputXml);
+            Console.WriteLine(result);
+
+        }
+
+        //Problem01
+
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+            IMapper mapper = InitializeAutoMapper();
+            XmlHelper xmlHelper = new XmlHelper();
+
+            ImportUserDto[] userDtos =
+                xmlHelper.Deserialize<ImportUserDto[]>(inputXml, "Users");
+
+            ICollection<User> validUsers = new HashSet<User>();
+
+            foreach(ImportUserDto userDto in userDtos)
+            {
+                if (string.IsNullOrEmpty(userDto.FirstName) || string.IsNullOrEmpty(userDto.LastName))
+                {
+                    continue;
+                }
+
+                User user = mapper.Map<User>(userDto);
+                validUsers.Add(user);
+            }
+            context.Users.AddRange(validUsers);
+            context.SaveChanges();
+            return $"Successfully imported {validUsers.Count}";
+
+        } 
+
+        private static IMapper InitializeAutoMapper()
+        => new Mapper(new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<ProductShopProfile>();
+        }));
+    }
+}
