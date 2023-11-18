ea42f6b6c0bd20133ce3416a840764c4a52be122
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-18 17:51:28 +0200

    Test

---
+++ 10.Exam Preparation/01.C# DB Advanced Retake Exam - 15 August 2022/01. Model Definition/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Trucks.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server = DESKTOP-EFFK5Q4;Database=Trucks;Trusted_Connection=True";
+    }
+}
---
+++ 10.Exam Preparation/01.C# DB Advanced Retake Exam - 15 August 2022/01. Model Definition/Data/Models/ClientTruck.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trucks.Data.Models
+{
+    public class ClientTruck
+    {
+        [ForeignKey(nameof(Client))]
+        public int ClientId  { get; set; }
+
+        //navigation property
+        public virtual Client Client { get; set; } = null!;
+
+        [ForeignKey(nameof(Truck))]
+        public int TruckId { get; set; }
+
+        //navigation property
+        public virtual Truck Truck { get; set; } = null!;
+    }
+}
