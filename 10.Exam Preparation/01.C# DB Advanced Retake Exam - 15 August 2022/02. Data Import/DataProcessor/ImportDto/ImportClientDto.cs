9c765d9842b05156de15703c176d198af3ccf907
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-18 17:51:53 +0200

    Test

---
+++ 10.Exam Preparation/01.C# DB Advanced Retake Exam - 15 August 2022/02. Data Import/DataProcessor/ImportDto/ImportClientDto.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trucks.Common;
+
+namespace Trucks.DataProcessor.ImportDto
+{
+    public class ImportClientDto
+    {
+        [Required]
+        [MinLength(ValidationConstants.ClientNameMinLength)]
+        [MaxLength(ValidationConstants.ClientNameMaxLength)]
+        [JsonProperty("Name")]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [MinLength(ValidationConstants.ClientNationalityMinLength)]
+        [MaxLength(ValidationConstants.ClientNationalityMaxLength)]
+        [JsonProperty("Nationality")]
+        public string Nationality { get; set; } = null!;
+
+        [JsonProperty("Type")]
+        public string Type { get; set; } = null!;
+
+        [JsonProperty("Trucks")] //-> в Jason може 
+        public int[] TruckIds { get; set; }  
+    }
+}
---
+++ 10.Exam Preparation/01.C# DB Advanced Retake Exam - 15 August 2022/02. Data Import/DataProcessor/ImportDto/ImportTruckDto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Trucks.Common;
+
+namespace Trucks.DataProcessor.ImportDto
+{
+    [XmlType("Truck")]
+    public class ImportTruckDto
+    {
+        
+        [XmlElement("RegistrationNumber")]
+        [MinLength(ValidationConstants.RegistrationNumberLength)]
+        [MaxLength(ValidationConstants.RegistrationNumberLength)]
+        [RegularExpression(ValidationConstants.TruckRegistrationNumberRegix)]
+        public string? RegistrationNumber { get; set; }
+
+        [XmlElement("VinNumber")]
+        [Required]
+        [MinLength(ValidationConstants.VinNumberMaxLength)]
+        [MaxLength(ValidationConstants.VinNumberMaxLength)]
+        public string VinNumber { get; set; } = null!;
+
+        [XmlElement("TankCapacity")]
+        [Range(ValidationConstants.TruckTankCapacityMinValue,ValidationConstants.TruckTankCapacityMaxValue)]
+        public int  TankCapacity  { get; set; }
+
+        [XmlElement("CargoCapacity")]
+        [Range(ValidationConstants.TruckCargoCapacityMinValue,ValidationConstants.TruckCargoCapacityMaxValue)]
+        public int CargoCapacity { get; set; }
+
+        [XmlElement("CategoryType")]
+        [Range(ValidationConstants.TruckCategoryTypeMinValue, ValidationConstants.TruckCategoryTypeMaxValue)]
+        public int CategoryType { get; set; }
+
+        [XmlElement("MakeType")]
+        [Range(ValidationConstants.TruckMakeTypeMinValue, ValidationConstants.TruckMakeTypeMinValue)]
+        public int MakeType { get; set; }
+    }
+}
