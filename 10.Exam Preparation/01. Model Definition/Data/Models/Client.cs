09144019509c911e713e185951be2d415f13249d
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-18 17:53:28 +0200

    Test

---
+++ 10.Exam Preparation/01. Model Definition/Data/Models/Client.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trucks.Common;
+
+namespace Trucks.Data.Models
+{
+    public class Client
+    {
+        public Client()
+        {
+            ClientsTrucks = new HashSet<ClientTruck>();
+        }
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(ValidationConstants.ClientNameMaxLength)]
+        public string Name { get; set; } = null!; // няма да има null стойности 
+
+        [Required]
+        [MaxLength(ValidationConstants.ClientNationnalittityMaxLength)]
+        public string Nationality { get; set; } = null!;
+
+        [Required]
+        public string Type { get; set; } = null!;
+
+       public virtual ICollection<ClientTruck> ClientsTrucks { get; set; } = null!;
+    }
+}
---
+++ 10.Exam Preparation/01. Model Definition/Data/Models/Truck.cs
@@ -0,0 +1,47 @@
+﻿using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using System;
+
+namespace Trucks.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using Trucks.Common;
+    using Trucks.Data.Models.Enums;
+
+    public class Truck
+    {
+        public Truck()
+        {
+            ClientsTrucks = new HashSet<ClientTruck>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(ValidationConstants.RegistrationNumberLength)]
+        public string? RegistrationNumber { get; set; }
+
+        [Required]
+        [MaxLength(ValidationConstants.VinNumberMaxLength)]
+        public string VinNumber { get; set; } = null!;
+
+        public int TankCapacity { get; set; }
+
+        public int CargoCapacity { get; set; }
+
+        public CategoryType CategoryType { get; set; }
+
+        public MakeType MakeType { get; set; }
+
+        [ForeignKey(nameof(Despatcher))] //-> принадлежи на навигационното пропърти и така ги навързваме 
+        public int DespatcherId { get; set; }
+
+        //navigation property
+
+        public virtual Despatcher Despatcher { get; set; } = null!;
+
+        public virtual ICollection<ClientTruck> ClientsTrucks { get; set; } = null!;
+
+
+    }
+}
