383cfdc8ee98a8dcd84504653be90a848e05535c
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-18 17:52:47 +0200

    Test

--- 10.Exam Preparation/01.C# DB Advanced Retake Exam - 15 August 2022/02. Data Import/Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Trucks.Data
-{
-    public static class Configuration
-    {
-        public static string ConnectionString = @"Server = DESKTOP-EFFK5Q4;Database=Trucks;Trusted_Connection=True";
-    }
-}
