757d6d9856fb9004ee2f5313b9792b43d518f7c3
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-21 21:37:55 +0200

    Test

---
+++ 13.Exam Preparation-11 April 2023/01. Model Definition/DataProcessor/Serializer.cs
@@ -0,0 +1,19 @@
+﻿namespace Invoices.DataProcessor
+{
+    using Invoices.Data;
+    using System;
+
+    public class Serializer
+    {
+        public static string ExportClientsWithTheirInvoices(InvoicesContext context, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ExportProductsWithMostClients(InvoicesContext context, int nameLength)
+        {
+
+            throw new NotImplementedException();
+        }
+    }
+}
