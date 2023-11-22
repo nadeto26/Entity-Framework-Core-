d74eb1fff02fefaf89c72cea15323bd36ffef858
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-22 19:40:37 +0200

    Test

---
+++ 14.Exam Preparation -01 April 2023/03. Data Export/Data/Configuration.cs
@@ -0,0 +1,9 @@
+﻿namespace Boardgames.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=DESKTOP-EFFK5Q4;
+        Database=Boardgames;Integrated Security=True;Encrypt=False";
+
+    }
+}
