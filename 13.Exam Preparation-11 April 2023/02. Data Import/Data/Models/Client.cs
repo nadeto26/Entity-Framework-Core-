3e14ec633645bbd23451f2530ecf491c8cc72828
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-23 17:21:08 +0200

    Test

---
+++ 13.Exam Preparation-11 April 2023/02. Data Import/Data/Models/Client.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Invoices.Data.Models
+{
+    public class Client
+    {
+        public Client()
+        {
+            Invoices = new HashSet<Invoice>();
+            Addresses = new HashSet<Address>();
+            ProductsClients = new HashSet<ProductClient>();
+        }
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(25)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [MaxLength(15)]
+        public string NumberVat { get; set; } = null!;
+
+        public virtual ICollection<Invoice> Invoices { get; set; } = null!;
+
+        public virtual ICollection<Address> Addresses { get; set; } = null!;
+
+        public virtual ICollection<ProductClient> ProductsClients { get; set; } = null!;
+    }
+
+}
---
+++ 13.Exam Preparation-11 April 2023/02. Data Import/DataProcessor/ImportDto/ImportProductDto.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Invoices.DataProcessor.ImportDto
+{
+    public class ImportProductDto
+    {
+        [Required]
+        [MaxLength(30)]
+        [MinLength(9)]
+        [JsonProperty("Name")]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [Range(5_00, 1000_00)]
+        [JsonProperty("Price")]
+        public decimal Price { get; set; }
+
+        [Required]
+        [JsonProperty("CategoryType")]
+        public int CategoryType { get; set; }
+
+        [JsonProperty("Clients")]
+        public int[] ClientsIds { get; set; }
+    }
+
+
+}
