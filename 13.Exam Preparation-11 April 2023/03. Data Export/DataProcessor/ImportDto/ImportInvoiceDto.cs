3776a5fea5a016c3e530a14ba20ee95d6b8b7d7b
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-23 17:21:34 +0200

    Test

---
+++ 13.Exam Preparation-11 April 2023/03. Data Export/DataProcessor/ImportDto/ImportInvoiceDto.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Invoices.DataProcessor.ImportDto
+{
+    public class ImportInvoiceDto
+    {
+        [Required]
+        [Range(1_000_000_000, 1_500_000_000)]
+        [JsonProperty("Number")]
+        public int Number { get; set; }
+
+        [Required]
+        [JsonProperty("IssueDate")]
+        public DateTime IssueDate { get; set; }
+
+        [Required]
+        [JsonProperty("DueDate")]
+        public DateTime DueDate { get; set; }
+
+        [Required]
+        [JsonProperty("Amount")]
+        public decimal Amount { get; set; }
+
+        [Required]
+        [JsonProperty("CurrencyType")]
+        public int CurrencyType { get; set; }
+
+        [JsonProperty("ClientId")]
+        public int ClientId { get; set; }
+    }
+
+}
