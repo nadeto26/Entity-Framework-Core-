b9d4561d014d467058a5b9155a13c6b1e42976e7
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-24 18:49:29 +0200

    Test

---
+++ 15.Exam Preparation- 16 Dec 2021/01. Model Definition/Data/Models/Country.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artillery.Data.Models
+{
+    public class Country
+    {
+        public Country()
+        {
+            CountriesGuns = new HashSet<CountryGun>();
+        }
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(60)]
+        public string CountryName { get; set; } = null!;
+
+        [Required]
+        [MaxLength(10_000_000)]
+        public int ArmySize { get; set; }
+
+        public virtual ICollection<CountryGun> CountriesGuns { get; set; }
+    }
+
+
+}
---
+++ 15.Exam Preparation- 16 Dec 2021/01. Model Definition/Data/Models/Shell.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artillery.Data.Models
+{
+    public class Shell
+    {
+        public Shell()
+        {
+            Guns = new HashSet<Gun>();
+        }
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(1_680)]
+        public double ShellWeight { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        public string Caliber { get; set; } = null!;
+
+        public virtual ICollection<Gun> Guns { get; set; }
+    }
+
+}
