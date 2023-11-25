5611937f3ac332a0b8b339492425f5e2ab492028
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-25 15:42:54 +0200

    Test

---
+++ 16.Exam Prepation- 04 Dec 2021/03. Data Export/Data/Models/Theatre.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theatre.Data.Models
+{
+    public class Theatre
+    {
+        public Theatre()
+        {
+            Tickets = new HashSet<Ticket>();
+        }
+        [Key]
+        public int Id  { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [MinLength(4)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [MinLength(1)]
+        [MaxLength(10)]
+        public sbyte NumberOfHalls { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        [MaxLength(30)] 
+        public string Director { get; set; } = null!;
+
+        public virtual ICollection<Ticket> Tickets { get; set; } = null!;
+    }
+}
---
+++ 16.Exam Prepation- 04 Dec 2021/03. Data Export/DataProcessor/Serializer.cs
@@ -0,0 +1,50 @@
+﻿namespace Theatre.DataProcessor
+{
+    using Newtonsoft.Json;
+    using System;
+    using Theatre.Data;
+
+    public class Serializer
+    {
+        public static string ExportTheatres(TheatreContext context, int numbersOfHalls)
+        {
+            var result = context.Theatres.ToArray().Where(x => x.NumberOfHalls >= numbersOfHalls &&
+            x.Tickets.Count() >= 20)
+            .Select(x => new
+            {
+                Name = x.Name,
+                Halls = x.NumberOfHalls,
+                TotalIncome = x.Tickets.Where(x => x.RowNumber <= 5).Sum(x => x.Price),
+                Tickets = x.Tickets.Where(x => x.RowNumber <= 5).Select(t => new
+                {
+                    Price = t.Price,
+                    RowNumber = t.RowNumber
+                })
+                .OrderByDescending(p => p.Price)
+                .ToArray()
+            })
+            .OrderByDescending(h => h.Halls)
+            .ThenBy(n => n.Name);
+
+
+            return JsonConvert.SerializeObject(result, Formatting.Indented);
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+    //public static string ExportPlays(TheatreContext context, double raiting)
+    //{
+    //    throw new NotImplementedException();
+    //}
+}
+
+   
+
