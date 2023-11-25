8fc996a89a96a4ee1343ebe327b4c831f5235456
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-25 15:42:08 +0200

    Test

---
+++ 16.Exam Prepation- 04 Dec 2021/01. Model Defition/Data/Models/Cast.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theatre.Data.Models
+{
+    public class Cast
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        [MaxLength(30)]
+        public string FullName { get; set; } = null!;
+
+        [Required]
+        public bool IsMainCharacter  { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\+44-\d{2}-\d{3}-\d{4}$")]
+        public string PhoneNumber { get; set; } = null!;
+
+        [Required]
+        [ForeignKey(nameof(Play))]
+        public int PlayId { get; set; }
+
+        public virtual Play Play { get; set; } = null!;
+    }
+}
---
+++ 16.Exam Prepation- 04 Dec 2021/01. Model Defition/DataProcessor/ImportDto/ImportPlayDto.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Theatre.Data.Models.Enums;
+
+namespace Theatre.DataProcessor.ImportDto
+{
+    [XmlType("Play")]
+    public class ImportPlayDto
+    {
+        [MinLength(4)]
+        [MaxLength(50)]
+        [Required]
+        [XmlElement("Title")]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [MinLength(1)]
+        [XmlElement("Duration")]
+        public TimeSpan Duration { get; set; }
+
+        [Required]
+        [MinLength(0_00)]
+        [MaxLength(10_00)]
+        [XmlElement("Rating")]
+        public float Rating { get; set; }
+
+        [Required]
+        [XmlElement("Genre")]
+        public Genre Genre { get; set; }
+
+        [Required]
+        [MaxLength(700)]
+        [XmlElement("Description")]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        [MinLength(4)]
+        [MaxLength(30)]
+        [XmlElement("Screenwriter")]
+        public string Screenwriter { get; set; } = null!;
+    }
+}
