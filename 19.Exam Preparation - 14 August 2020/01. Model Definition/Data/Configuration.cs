78d1395b574739134529bcf824aad07a769a3042
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-28 18:56:54 +0200

    Test

---
+++ 19.Exam Preparation - 14 August 2020/01. Model Definition/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace SoftJail.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=DESKTOP-EFFK5Q4;Database=SoftJail;Integrated Security=True;Encrypt=False;";
+    }
+}
---
+++ 19.Exam Preparation - 14 August 2020/01. Model Definition/Data/Models/Department.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftJail.Data.Models
+{
+    public class Department
+    {
+        public Department()
+        {
+            this.Cells = new HashSet<Cell>();
+        }
+
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(GlobalConstants.DepartmentNameMaxLength)]
+        public string Name { get; set; }
+
+        public virtual ICollection<Cell> Cells { get; set; }
+    }
+}
