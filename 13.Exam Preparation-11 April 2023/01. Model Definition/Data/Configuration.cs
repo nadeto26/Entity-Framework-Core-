405e6d3953ac70c9f954aea5b43bcc56a204a7ae
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-29 17:16:10 +0200

    Test

--- 13.Exam Preparation-11 April 2023/01. Model Definition/Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Invoices.Data
-{
-    public static class Configuration
-    {
-        public static string ConnectionString = @"Server=DESKTOP-EFFK5Q4;Database=Invoices;Integrated Security=True;Encrypt=False";
-    }
-}
