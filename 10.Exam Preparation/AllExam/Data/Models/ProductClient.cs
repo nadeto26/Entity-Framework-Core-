783106ed45338724f594ca32335791f217aabdfb
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-11-30 11:13:20 +0200

    Test

---
+++ 10.Exam Preparation/AllExam/Data/Models/ProductClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Invoices.Data.Models
+{
+    public class ProductClient
+    {
+        [ForeignKey(nameof(Product))]
+        public int ProductId { get; set; }
+
+        public virtual Product Product { get; set; } = null!;
+
+
+        [ForeignKey(nameof(Client))]
+        public int ClientId { get; set; }
+
+        public virtual Client Client { get; set; } = null!;
+    }
+
+}
