4bf77b8f0b1bb7cb6bedab40873d7d88be20a453
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-12-01 14:37:01 +0200

    Test

---
+++ 11.Exam Preparation/02.Another Solution/Data/Models/CountryGun.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artillery.Data.Models
+{
+    public class CountryGun
+    {
+        public int CountryId { get; set; }
+        public virtual Country Country { get; set; }
+
+        public int GunId { get; set; }
+        public virtual Gun Gun { get; set; }
+    }
+}
---
+++ 11.Exam Preparation/02.Another Solution/Data/Models/Shell.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artillery.Data.Models
+{
+    public class Shell
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public double ShellWeight { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Caliber { get; set; }
+
+        public virtual ICollection<Gun> Guns { get; set; } = new HashSet<Gun>();
+    }
+}
