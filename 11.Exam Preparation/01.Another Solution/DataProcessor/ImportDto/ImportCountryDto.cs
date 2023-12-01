49525320ca3614bfd1f394d2f1571d9535e9a7a6
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-12-01 14:37:27 +0200

    Test

---
+++ 11.Exam Preparation/01.Another Solution/DataProcessor/ImportDto/ImportCountryDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Artillery.DataProcessor.ImportDto
+{
+    [XmlType("Country")]
+    public class ImportCountryDto
+    {
+        [Required]
+        [MaxLength(60)]
+        [MinLength(4)]
+        [XmlElement("CountryName")]
+        public string CountryName { get; set; }
+
+        [Range(50_000, 10_000_000)]
+        [XmlElement("ArmySize")]
+        public int ArmySize { get; set; }
+    }
+}
---
+++ 11.Exam Preparation/01.Another Solution/DataProcessor/Serializer.cs
@@ -0,0 +1,43 @@
+﻿
+namespace Artillery.DataProcessor
+{
+    using Artillery.Data;
+    using Artillery.Data.Models.Enums;
+    using Newtonsoft.Json;
+
+    public class Serializer
+    {
+        public static string ExportShells(ArtilleryContext context, double shellWeight)
+        {
+            var shells = context.Shells
+                .Where(s=>s.ShellWeight>=shellWeight)
+                .Select(s=> new
+                {
+                    ShellWeight = s.ShellWeight,
+                    Caliber = s.Caliber,
+                    Guns = s.Guns.Where(x => ((int)x.GunType) == 3)
+                    .Select(g=> new
+                    {
+                       GunType = g.GunType,
+                       GunWeight = g.GunWeight,
+                        BarrelLength = g.BarrelLength,
+                        Range = g.Range > 3000 ? "Long-range" : "Regular range",
+                    })
+                    .OrderByDescending(x => x.GunWeight)
+                    .ToArray()
+                })
+                .OrderBy(x => x.ShellWeight)
+                .ToArray();
+
+            var json = JsonConvert.SerializeObject(shells, Formatting.Indented);
+            return json;
+      
+                
+        }
+
+        public static string ExportGuns(ArtilleryContext context, string manufacturer)
+        { 
+            throw new NotImplementedException();
+        }
+    }
+}
