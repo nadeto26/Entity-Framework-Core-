b44455f504641d5bbf2ea5c06f845a1234b9ae42
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-12-01 14:35:49 +0200

    Test

---
+++ 11.Exam Preparation/03.Another Solution/Data/Models/Country.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artillery.Data.Models
+{
+    public class Country
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(60)]
+        public string CountryName { get; set; }
+
+        public int ArmySize { get; set; }
+        public virtual ICollection<CountryGun> CountriesGuns { get; set; } = new HashSet<CountryGun>();
+    }
+}
