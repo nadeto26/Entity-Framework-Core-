57c34adfdce564eed617c99626ad0f3b6f4f8250
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-12-04 16:52:13 +0200

    Test

---
+++ 10.Exam Preparation/Auto-Mapping-Objects-Exercises-FastFood-6.0/FastFood.Services.Mapping/FastFoodProfile.cs
@@ -0,0 +1,20 @@
+﻿namespace FastFood.Core.MappingConfiguration
+{
+    using Models;
+    using AutoMapper;
+    
+    using ViewModels.Positions;
+
+    public class FastFoodProfile : Profile
+    {
+        public FastFoodProfile()
+        {
+            //Positions
+            CreateMap<CreatePositionInputModel, Position>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.PositionName));
+
+            CreateMap<Position, PositionsAllViewModel>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name));
+        }
+    }
+}
---
+++ 10.Exam Preparation/Auto-Mapping-Objects-Exercises-FastFood-6.0/FastFood.Web.ViewModels/Orders/CreateOrderViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace FastFood.Core.ViewModels.Orders
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class CreateOrderViewModel
+    {   
+        public List<int> Items { get; set; } = null!;
+
+        public List<int> Employees { get; set; } = null!;
+
+         
+    }
+}
